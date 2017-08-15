90370ee2f466101e9a7da8ecd24572b9841ddc73
mgladkova <gladkovamaria14@gmail.com>
2017-08-15 22:41:15 +0300

    Added pandas dataframe output for database

--- jacobs_university_contribution/database_ads_publications.py
+++ jacobs_university_contribution/database_ads_publications.py
@@ -9,12 +9,15 @@ import json
 import requests
 import re
 import io
+import pandas as pd
 from feature_class import Feature
 
 
 # In[ ]:
 
 
+# routine to read information from json file
+# creates a Feature instance with extracted data
 def feature_extract(obj):
         if '__type__' in obj and obj['__type__'] == 'Feature':
             return Feature(obj['name'], obj['id'], obj['polygon_coordinates'], obj['publications'])
@@ -24,6 +27,7 @@ def feature_extract(obj):
 # In[ ]:
 
 
+# routine to parse json file and fetch definition of each feature
 def parse_json_stream(text):
     list_idx = [m.start() for m in re.finditer('}\n', text)]
     list_objs = []
@@ -54,35 +58,57 @@ with open(json_name) as fin:
 # In[ ]:
 
 
+# write a json file with extended list of publications for each feature
+# publications are requested from ADS API
+# list of features is stored in features variable
 with io.open('features_extended.json', 'w', encoding='utf-8') as fout:
     for feature in features:
-        print(feature.name + " extended")
+        print(feature.name)
+        # request ADS API for publications based on feature name
+        # store objects of type Article
         papers = []
-        
         try:
-            papers = list(ads.SearchQuery(q=feature.name, sort="year", fl=['title', 'first_author', 'year', 'pub', 'bibcode']))
+            papers = list(ads.SearchQuery(q=feature.name, fl=['title', 'first_author', 'year', 'pub', 'bibcode']))
         except (ads.exceptions.APIResponseError, ads.exceptions.SolrResponseParseError) as e:
             "Error: {}".format(e)
             continue
         
         citation_str = []
+        # extract several fields from each publication
+        # whole list of fields can be found in https://github.com/adsabs/adsabs-dev-api/blob/master/search.md#fields
         for paper in papers:
             if (type(paper.title).__name__ == "NoneType"):
                 p_title = "Unknown"
             else:    
                 p_title = paper.title[0]
+                
             p_author = paper.first_author
             p_year = str(paper.year)
             
             if (type(paper.pub).__name__ == "NoneType"):
                 p_pub = ", "
             else:
-                #print(paper.pub)
                 p_pub = " : " + paper.pub + ", "
+            # combine all fields in a comprehensible string
             citation_str.append(p_title + " by " + p_author + p_pub + p_year)
+        # extend feature's list of publications 
         feature.addPublications(citation_str)
+        #write extended feature info to a new json file
         fout.write(str(json.dumps(feature.dump(), ensure_ascii=False, indent=4))+"\n")
 fout.close()
+
+
+# In[ ]:
+
+
+pd.set_option('display.max_colwidth', -1)
+df = pd.DataFrame([feature.to_dict() for feature in features])
+
+
+# In[ ]:
+
+
+#print(df["publications"][0])
 
 
 # In[ ]:
--- jacobs_university_contribution/feature_class.py
+++ jacobs_university_contribution/feature_class.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[2]:
 
 
 # an encapsulation of data retrieved about a geographical feature
@@ -16,13 +16,21 @@ class Feature:
         
     # in case # publications for a feature > 1
     def addPublications(self, nEntry):
-        self.publ.extend(nEntry)
+        (self.publ).extend(nEntry)
+    
     # writing routine to a json file
     def dump(self):
         return {'__type__': type(self).__name__, 'name': self.name,
                                'id': self.id,
                                'polygon_coordinates': self.pcoord,
-                               'publications': self.publ}
+                               'publications': self.publ }
+    def to_dict(self):
+        return {
+            'name' : self.name,
+            'id' : self.id,
+            'polygon_coordinates' : self.pcoord,
+            'publications' : self.publ
+        }
 
 
 # In[ ]:
